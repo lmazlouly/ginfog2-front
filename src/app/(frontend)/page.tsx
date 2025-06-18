2fbcec2e4a709482a2a931dfbd6ed32ae9c33850
Soufianinho <soufianmeskin5@gmail.com>
2025-06-18 01:38:28 +0100

    Update page.tsx

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "EcoSys ",
+  title: "EcoSys",
   description: "Explore evidence-based teaching strategies for your classroom",
 };
 
